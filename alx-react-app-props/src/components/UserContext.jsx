7659dd9e4a2ec2a6db015cd8865708e296e6f1f6
vincent-eboya613 <vincentkidavi@gmail.com>
2024-11-09 14:56:26 +0300

    Delete alx-react-app-props/src/components/UserContext.jsx

--- alx-react-app-props/src/components/UserContext.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// Create a UserContext using React.createContext()
-const UserContext = React.createContext();
-
-// Export the context to be used in other components
-export default UserContext;
