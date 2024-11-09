c4ecc6b543d2329d2ccdf6967cc3d8823eef5d13
vincent-eboya613 <vincentkidavi@gmail.com>
2024-11-09 14:57:01 +0300

    the updated user context js file

---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// Create a UserContext using React.createContext()
+const UserContext = React.createContext();
+
+// Export the context to be used in other components
+export default UserContext;
