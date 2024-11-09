32fd0a4c89ab359c401f1cc66bf24fedba047f18
vincent-eboya613 <vincentkidavi@gmail.com>
2024-11-09 13:10:27 +0300

    styled files

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+function Footer () {
+  return (
+<footer style={{
+  backgroundColor: '#213',
+  color: '#333',
+  padding: '20px 0',
+  textAlign: 'center',
+  fontFamily: 'Arial, sans-serif',
+  position: 'relative',
+  boxShadow: '0 -2px 10px rgba(0, 0, 0, 0.2)',
+}}>
+  <p style={{
+      margin: 0,
+      fontSize: '14px',
+      color: 'rgba(255, 255, 255, 0.7)',
+  }}>
+      &copy; 2023 City Lovers
+  </p>
+</footer>
+
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent () {
+  return (
+      <main  style={{backgroundImage: `url("https://images.pexels.com/photos/1239162/pexels-photo-1239162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")` 
+          , width:'110%', height:'200vh', backgroundSize:'cover', backgroundPosition:'center'
+      }}>
+          <p style={{fontSize:'50px' , color:'aquamarine'}}>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+  );
+}
+
+export default MainContent;
