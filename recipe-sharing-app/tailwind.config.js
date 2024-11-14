2654b55d956edd86c9f7db14a380cac2fda94615
vincent-eboya613 <vincentkidavi@gmail.com>
2024-11-14 08:16:03 +0300

    Delete recipe-sharing-app/tailwind.config.js

--- recipe-sharing-app/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
