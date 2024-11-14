30922372ee410aa27e747014d79ed434e35fd0f8
vincent-eboya613 <vincentkidavi@gmail.com>
2024-11-14 09:06:45 +0300

    recipe form file

---
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -0,0 +1,37 @@
+import { useRecipeStore } from "./recipeStore";
+import { useState } from "react";
+
+const EditRecipeForm = ({ recipe }) => {
+    const updateRecipe = useRecipeStore(state => state.updateRecipe);
+    const [title, setTitle] = useState(recipe.title);
+    const [description, setDescription] = useState(recipe.description);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const updatedRecipe = {
+            ...recipe,
+            title,
+            description
+        };
+        updateRecipe(recipe);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Title"
+            />
+            <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Description"
+            />
+        <button type="submit">Edit Recipe</button>
+      </form>
+    );
+}
+
+export default EditRecipeForm
