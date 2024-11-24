8e9f5ab78ea0c7c0aa77af45670f1c415fda9f1d
vincent-eboya613 <vincentkidavi@gmail.com>
2024-11-24 23:16:45 +0300

    component files all have been added

---
+++ react-router-advanced/src/components/Blogpost.jsx
@@ -0,0 +1,11 @@
+
+// src/pages/BlogPost.jsx
+import { useParams } from "react-router-dom";
+
+function BlogPost() {
+  const { postId } = useParams();
+
+  return <div>Blog Post ID: {postId}</div>;
+}
+
+export default BlogPost;
---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,9 @@
+
+// src/pages/ProfileDetails.jsx
+
+function ProfileDetails() {
+    return <div>Profile Details</div>;
+  }
+  
+  export default ProfileDetails;
+  
