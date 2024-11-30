56880c8d997dca727d26b2c94d313872dc8ab1b9
vincent-eboya613 <vincentkidavi@gmail.com>
2024-11-30 16:59:18 +0300

    updating the jsx file

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,8 +1,3 @@
-
-import { QueryClient, QueryClientProvider } from "react-query";
-import PostsComponent from "./PostsComponent"; // Adjust the import path if necessary
-
-// Create a client
 import './App.css'
 import UserProfile from './components/UserProfile'
 
